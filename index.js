236f61e563087b81b74dcc9b9951b8617c800bd9
Serge Droganov <serge@droganov.ru>
2016-03-01 12:16:59 +0000

    connect => connectRacer

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 var serverRequire = require("racer/lib/util").serverRequire;
 
 var bundle = serverRequire( module, "./lib/bundle" );
-var Connect = require( "./lib/connect" );
+var connectRacer = require( "./lib/connect" );
 var connectClient = require( "./lib/connect-client" );
 var match = require( "./lib/match" );
 var Provider = require( "./lib/provider" );
 
 module.exports = {
   bundle: bundle,
-  Connect: Connect,
+  connectRacer: connectRacer,
   connectClient: connectClient,
   match: match,
   Provider: Provider
